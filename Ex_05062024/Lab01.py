160da8feafa6c97a7e54628701918705db79c7c7
ErHimanth <Himanth5911#gmail.com>
2024-06-12 22:00:01 +0530

    adding Lab002 file

---
+++ Ex_05062024/Lab01.py
@@ -0,0 +1,4 @@
+print("hello", "world", 123, True, 3.14, end="\t")
+print("hello", "world", 123, True, 3.14, )
+print("hello", "world", 123, True, 3.14, end="_")
+print("hello", "world", 123, True, 3.14, )
---
+++ Ex_05062024/Lab02.py
@@ -0,0 +1,20 @@
+age = 65
+name = "himanth"
+pi = 3.14
+isMale = True
+complexNumber = 2 + 3j
+# Dynamically typed
+age = "sixty five"
+print(type(age))
+print(type(name))
+print(type(isMale))
+print(type(pi))
+print(complexNumber.real)
+print(complexNumber.imag)
+a, b, c, = 10, 20, 30
+print(a + b + c)
+#name = input("enter ur name")
+#print("your name is", name)
+first_name = input("enter ur 1stname")
+last_name = input("enter ur Lstname")
+print("your 1stname is", first_name, "your Lststname is", last_name)
