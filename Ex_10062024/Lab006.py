8ab71eb11a465de0b784279408976e7f875a39c9
ErHimanth <Himanth5911#gmail.com>
2024-06-17 18:33:38 +0530

    adding  file

---
+++ Ex_10062024/Lab006.py
@@ -0,0 +1,35 @@
+# operatora
+# assignment operators
+name = "himanth"
+print(name)
+# comparison or compare operator (==)
+v1 = (1 == True)
+v2 = (0 == False)
+# Unary operator
+print(v1, v2)
+age = +65
+print(age)
+lalli = 77
+print(-lalli)
+# Not operator : it gives opposite result to given statment. it only works with boolean
+a = True
+print(not a)
+# Arithmetic operator: +,-,/,%
+a = 7
+b = 4
+print(a + b)
+print(b - a)
+print(a * b)
+print(b / a)
+print(b // a)
+print(b % a)
+print(4 ** 4)
+# Logical operator <, >, <=, >=
+print(a > b)
+print(b > a)
+print(a >= b)
+print(a <= b)
+f = False
+t = True
+print(f and t)
+print(f or t)
